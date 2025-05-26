e833704eba1e649301f917337a45a24ebbb20426
JemalJaquavius <acrause@learn68.ca>
2025-05-25 20:01:10 -0700

    Update JAVA.java

--- JAVA.java
+++ JAVA.java
@@ -1,8 +1,12 @@
 public class Numbers {
+  static void myMethod() {
+    System.out.println("This is a method that got executed.");
+  }
   public static void main(String[] args) {
     int number = 5;
   System.out.println("This is Java.");
   System.out.println("It is a coding language I am currently learning.");
   System.out.println("I like the number " + number);
+  myMethod();
   }
 }
