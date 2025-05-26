3929ac24b921f30f424c81eda99efc9b7483847a
JemalJaquavius <acrause@learn68.ca>
2025-05-25 18:00:31 -0700

    JAVA.java

--- JAVA.java
+++ JAVA.java
@@ -1,4 +1,7 @@
 public class Main {
   public static void main(String[] args) {
-  System.out.println("This is Java");
+    int number = "5";
+  System.out.println("This is Java.");
+  System.out.println("It is a coding language I am currently learning.");
+    System.out.println("I like the number " + number);
   }
