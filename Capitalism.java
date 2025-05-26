6dfc4b6bb4cb91c7ba8ab2766f39a3a5212d0cad
JemalJaquavius <acrause@learn68.ca>
2025-05-25 20:18:06 -0700

    Create Capitalism.java

---
+++ Capitalism.java
@@ -0,0 +1,13 @@
+public class Main {
+  static void text() {
+    System.out.println("Capitalism is the best.")
+  }
+  public static void main(Sting[] args) {
+    string mOnEy = "Money is amazing.";
+    System.out.println("Many people don't know this, but capitalism is the best");
+    System.out.println(mOnEy)
+    if (10 + 10 = 20) {
+      System.out.println("Many people say it's corrupt, but Ricky DooHicky , the supposed \"lizard person\", says otherwise.")
+    }
+  }
+}
