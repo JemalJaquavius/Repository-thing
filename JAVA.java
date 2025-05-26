1d99cf047281073576a80cca9d49776a1c6599f0
JemalJaquavius <acrause@learn68.ca>
2025-05-25 19:21:48 -0700

    Update JAVA.java

--- JAVA.java
+++ JAVA.java
@@ -1,8 +1,8 @@
 public class Numbers {
   public static void main(String[] args) {
     int number = 5;
-  System.out.println("This is Java.");
-  System.out.println("It is a coding language I am currently learning.");
-    System.out.println("I like the number " + number);
+  System.out.println("This is Java."); //Declares what the programming language is
+  System.out.println("It is a coding language I am currently learning."); //Lets users know that I am new to this coding language
+  System.out.println("I like the number " + number); //This will write the text "I like the number " and then add a variable called 'number', which will print a 5
   }
 }
