2b875c9129cbf29818adb5261e11627df42ea9ac
JemalJaquavius <acrause@learn68.ca>
2025-05-25 20:22:47 -0700

    Update Capitalism.java

--- Capitalism.java
+++ Capitalism.java
@@ -1,9 +1,9 @@
 public class Main {
-  public static void main(Sting[] args) {
-    string money = "Money is amazing.";
+  public static void main(String[] args) {
+    String money = "Money is amazing.";
     System.out.println("Many people don't know this, but capitalism is the best");
     System.out.println(money);
-    if (10 + 10 = 20) {
+    if (10 + 10 == 20) {
       System.out.println("Many people say it's corrupt, but Ricky DooHicky , the supposed \"lizard person\", says otherwise.");
     }
   }
