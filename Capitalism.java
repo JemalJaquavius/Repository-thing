de2dab466f82fdda80f824abcabdbeae0d566fa9
JemalJaquavius <acrause@learn68.ca>
2025-05-25 20:20:57 -0700

    Update Capitalism.java

--- Capitalism.java
+++ Capitalism.java
@@ -1,13 +1,9 @@
 public class Main {
-  static void text() {
-    System.out.println("Capitalism is the best.")
-  }
   public static void main(Sting[] args) {
-    string mOnEy = "Money is amazing.";
+    string money = "Money is amazing.";
     System.out.println("Many people don't know this, but capitalism is the best");
-    System.out.println(mOnEy)
+    System.out.println(money);
     if (10 + 10 = 20) {
-      System.out.println("Many people say it's corrupt, but Ricky DooHicky , the supposed \"lizard person\", says otherwise.")
+      System.out.println("Many people say it's corrupt, but Ricky DooHicky , the supposed \"lizard person\", says otherwise.");
     }
   }
-}
