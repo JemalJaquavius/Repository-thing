2c9091e85df1531d512403845c4451a0607ed472
JemalJaquavius <acrause@learn68.ca>
2025-05-26 09:16:21 -0700

    Update Main.java

--- Main.java
+++ Main.java
@@ -8,5 +8,18 @@ public class Main {
   System.out.println("It is a coding language I am currently learning.");
   System.out.println("I like the number " + number);
   myMethod();
+  if (number + 5 == 10){
+    System.out.println("five and five make ten");
+  } else {
+    System.out.println("five and five do not make ten");
+  }
+  int x = 10;
+  int y = 4;
+  System.out.println(x + y);
+  if (x + y == 14) {
+    System.out.println("x and y are equal to 14");
+  } else {
+    System.out.println("x and y are not equal to 14")
+  }
   }
 }
