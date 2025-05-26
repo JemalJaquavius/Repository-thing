491d114433d244c141b4402dc4e9658fb6557204
JemalJaquavius <acrause@learn68.ca>
2025-05-26 09:17:48 -0700

    Update Capitalism.java

--- Capitalism.java
+++ Capitalism.java
@@ -5,6 +5,8 @@ public class Capitalism {
     System.out.println(money);
     if (10 + 10 == 20) {
       System.out.println("Many people say it's corrupt, but Ricky DooHicky , the supposed \"lizard person\", says otherwise.");
+    } else {
+      System.out.println(money + " It's true");
     }
   }
 }
