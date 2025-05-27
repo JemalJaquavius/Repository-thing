efc19cad527b438aa0e5aa9b271bf26b3ec45239
JemalJaquavius <acrause@learn68.ca>
2025-05-27 12:53:58 -0700

    Create File.py

---
+++ File.py
@@ -0,0 +1,9 @@
+print("This is a Python file")
+name = input("Enter a name:")
+print(5 + 5)
+x = 10
+y = 6
+if x + y == 16:
+  print("I like the number 16")
+else:
+  print("I don't like the number 16")
