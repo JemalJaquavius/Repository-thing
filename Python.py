7c693139beb287f090f8009d9b5b3ccaaa948eae
JemalJaquavius <acrause@learn68.ca>
2025-05-26 22:01:09 -0700

    Update Python.py

--- Python.py
+++ Python.py
@@ -6,4 +6,12 @@ else:
   print("a is not greater than b")
 print("This is a sentence that I have added for testing purposes")
 i = input("Is Larry the cucumber cool: ")
-print("So you said 
+print("So you said {i}, I disagree with that statement")
+
+x = 26
+print("If x + 5 = 31, what is the value of x?")
+y = input("Answer goes here: ")
+if y = x:
+ print("Correct answer!")
+else:
+ print("Incorrect answer")
