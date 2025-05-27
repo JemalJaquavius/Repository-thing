a6fb20f0da2a86f7ec31e0ca7e38ecfef70b276f
JemalJaquavius <acrause@learn68.ca>
2025-05-26 21:56:34 -0700

    Update Python.py

--- Python.py
+++ Python.py
@@ -4,3 +4,6 @@ if a > b:
   print("a is greater than b")
 else:
   print("a is not greater than b")
+print("This is a sentence that I have added for testing purposes")
+i = input("Is Larry the cucumber cool: ")
+print("So you said 
