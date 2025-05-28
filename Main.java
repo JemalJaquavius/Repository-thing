dd66bdb9332468c074b2f890c86a35792db5ebe3
JemalJaquavius <acrause@learn68.ca>
2025-05-27 20:08:04 -0700

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,13 +1,9 @@
-import java.time.LocalDateTime;
 public class Main {
-  static void myMethod() {
-    System.out.println("This is a method that got executed.");
-  }
   static void thisMethod() {
-    System.out.println("Another method that got executed");
-      if (8 + 2 == 10) {
-      System.out.println("I like math");
-      }
+    System.out.println("This is a method");
+  }
+  static void otherMethod() {
+    System.out.println("This is another method");
   }
   public static void main(String[] args) {
     int number = 5;
@@ -19,24 +15,8 @@ public class Main {
     System.out.println("five and five make ten");
   } else {
     System.out.println("five and five do not make ten");
-  myMethod();
-  int x = 10;
-  int y = 4;
-  System.out.println(x + y);
-  if (x + y == 14) {
-    System.out.println("x and y are equal to 14");
-  } else {
-    System.out.println("x and y are not equal to 14");
   }
-  LocalTime currentTime = LocalTime.now();
-  System.out.println(currentTime);
-  }
-  thisMethod();
-   if (x > y) {
-     System.out.println("x is greater than y");
-   } else if (x < y) {
-     System.out.println("y is greater than x");
-   } else {
-     System.out.println("x is x, y is y");
+    thisMethod();
+    System.out.println(x + " is a number");
    }
 }
